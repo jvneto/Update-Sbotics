26ba74f710ec9590aec1ce4dde6beb242501a2b9
Julio Cesar <60150667+jvneto@users.noreply.github.com>
2021-05-12 14:33:14 -0300

    v1.0.52

--- src/class/__DownloadSbotics.js
+++ src/class/__DownloadSbotics.js
@@ -12,43 +12,32 @@ const sBoticsDownload = () => {
   var gc = new GithubContent(gitOptions);
 
   const download = [
-    "W32/sBotics_Data/StreamingAssets/Addons/BlockEduc.exepackage.json",
-    "W32/sBotics_Data/StreamingAssets/Addons/BlockEduc.exe",
-    "W32/sBotics_Data/StreamingAssets/ColorTheme.json.zip",
+    // "W32/sBotics_Data/StreamingAssets/Addons/BlockEduc.exepackage.json",
+    // "W32/sBotics_Data/StreamingAssets/Addons/BlockEduc.exe",
+    // "W32/sBotics_Data/StreamingAssets/ColorTheme.json.zip",
     "W32/UnityPlayer.dll",
-    "W32/sBotics.exe",
+    // "W32/sBotics.exe",
     "W32/sBotics_Data/Managed/System.Data.dll",
     "W32/sBotics_Data/Managed/System.Windows.Forms.dll",
+    "W32/sBotics_Data/Managed/System.Security.dll",
+    "W32/sBotics_Data/Managed/System.Transactions.dll",
+    "W32/sBotics_Data/Managed/System.Web.dll",
   ];
 
   download.forEach((element) => {
-    console.log(element);
+    gc.file(element, function (err, file) {
+      if (err) return console.log(err);
+      const path = file.path;
+      const content = file.contents;
+      console.log(path);
+      console.log(content);
+      const desktopPath = homeDir + "/desktop/sbotics/";
+      fs.writeFile(desktopPath + path, content, (error) => {
+        console.log(error ? false : true);
+      });
+      console.log(new Date().getSeconds() - before);
+    });
   });
-
-  // const download = [
-  //   "W32/sBotics_Data/StreamingAssets/Addons/BlockEduc.exepackage.json",
-  //   "W32/sBotics_Data/StreamingAssets/Addons/BlockEduc.exe",
-  //   "W32/sBotics_Data/StreamingAssets/ColorTheme.json.zip",
-  //   "W32/UnityPlayer.dll",
-  //   "W32/sBotics.exe",
-  //   "W32/sBotics_Data/Managed/System.Data.dll",
-  //   "W32/sBotics_Data/Managed/System.Windows.Forms.dll",
-  // ];
-  // for (let index = 0; index < download.length; index++) {
-  //   const pathDownload = download[index];
-  //   gc.file(pathDownload, function (err, file) {
-  //     if (err) return console.log(err);
-  //     const path = file.path;
-  //     const content = file.contents;
-  //     console.log(path);
-  //     console.log(content);
-  //     const desktopPath = homeDir + "/desktop/sbotics/";
-  //     fs.writeFile(desktopPath + path, content, (error) => {
-  //       console.log(error ? false : true);
-  //     });
-  //     console.log(new Date().getSeconds() - before);
-  //   });
-  // }
 };
 
 sBoticsDownload();
