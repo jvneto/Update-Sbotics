12282c6f7a8a1b29b95979a52090fae6acb44adf
Julio Cesar <60150667+jvneto@users.noreply.github.com>
2021-05-12 10:11:54 -0300

    v1.0.8

--- main.js
+++ main.js
@@ -1,7 +1,4 @@
 const { app, BrowserWindow, TouchBar } = require("electron");
-const { path } = require("path");
-const { url } = require("url");
-
 let window;
 
 app.whenReady().then(() => {
@@ -12,11 +9,5 @@ app.whenReady().then(() => {
     backgroundColor: "#000",
   });
   // window.loadURL("about:blank");
-  window.loadURL(
-    url.format({
-      pathname: path.join(__dirname, "/src/html/newIndex.html"),
-      protocol: "file:",
-      slashes: true,
-    })
-  );
+  window.loadURL("file://" + __dirname + "/src/index.html");
 });
