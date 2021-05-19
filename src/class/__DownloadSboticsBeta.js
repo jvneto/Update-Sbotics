f5c372a16b376c14ca846c041906b9ebaf18e9ec
Julio Cesar <60150667+jvneto@users.noreply.github.com>
2021-05-19 01:37:33 -0300

    v1.0.255

--- src/class/__DownloadSboticsBeta.js
+++ src/class/__DownloadSboticsBeta.js
@@ -1,46 +1,13 @@
 var sBoticsDownloader = require("sbotics-downloader");
-
 const sbotics = new sBoticsDownloader({
   user: "sBotics",
   repository: "sBoticsBuilds",
   branch: "master",
-  externalDownload: false,
-  detailedAnswer: false,
-  wordsToRemove: ["W32/"],
-});
-sbotics.file(
-  "https://raw.githubusercontent.com/Txiag/sBotics/master/W32/sBotics_Data/Managed/UnityEngine.SpriteShapeModule.dll",
-  {
-    downloadMode: "external",
-    savePath: "sBotics_Data/Managed/UnityEngine.SpriteShapeModule.dll",
-  },
-  (err, resp) => {
-    if (err) return console.log(err);
-    console.log(resp);
-  }
-);
-sbotics.file("W32/sBotics.exe", (err, resp) => {
-  if (err) return console.log(err);
-  console.log(resp);
-});
-
-sbotics.file("W32/UnityPlayer.dll", (err, resp) => {
-  if (err) return console.log(err);
-  console.log(resp);
-});
-
-sbotics.file("W32/UnityCrashHandler32.exe", (err, resp) => {
-  if (err) return console.log(err);
-  console.log(resp);
-});
-
-sbotics.file("W32/sBotics_Data/data.unity3d", (err, resp) => {
-  if (err) return console.log(err);
-  console.log(resp);
 });
 
 sbotics.file(
-  "W32/sBotics_Data/StreamingAssets/ColorTheme.json.zip",
+  "W32/UnityPlayer.dll",
+  { savePath: "UnityPlayer.dll" },
   (err, resp) => {
     if (err) return console.log(err);
     console.log(resp);
@@ -48,9 +15,74 @@ sbotics.file(
 );
 
 sbotics.file(
-  "W32/sBotics_Data/StreamingAssets/Addons/BlockEduc.exe",
+  "W32/UnityCrashHandler32.exe",
+  { savePath: "UnityCrashHandler32.exe" },
   (err, resp) => {
     if (err) return console.log(err);
     console.log(resp);
   }
 );
+
+sbotics.file(
+  "W32/sBotics_Data/data.unity3d",
+  { savePath: "sBotics_Data/data.unity3d" },
+  (err, resp) => {
+    if (err) return console.log(err);
+    console.log(resp);
+  }
+);
+
+// const sbotics = new sBoticsDownloader({
+//   user: "sBotics",
+//   repository: "sBoticsBuilds",
+//   branch: "master",
+//   externalDownload: false,
+//   detailedAnswer: false,
+//   wordsToRemove: ["W32/"],
+// });
+// sbotics.file(
+//   "https://raw.githubusercontent.com/Txiag/sBotics/master/W32/sBotics_Data/Managed/UnityEngine.SpriteShapeModule.dll",
+//   {
+//     downloadMode: "external",
+//     savePath: "sBotics_Data/Managed/UnityEngine.SpriteShapeModule.dll",
+//   },
+//   (err, resp) => {
+//     if (err) return console.log(err);
+//     console.log(resp);
+//   }
+// );
+// sbotics.file("W32/sBotics.exe", (err, resp) => {
+//   if (err) return console.log(err);
+//   console.log(resp);
+// });
+
+// sbotics.file("W32/UnityPlayer.dll", (err, resp) => {
+//   if (err) return console.log(err);
+//   console.log(resp);
+// });
+
+// sbotics.file("W32/UnityCrashHandler32.exe", (err, resp) => {
+//   if (err) return console.log(err);
+//   console.log(resp);
+// });
+
+// sbotics.file("W32/sBotics_Data/data.unity3d", (err, resp) => {
+//   if (err) return console.log(err);
+//   console.log(resp);
+// });
+
+// sbotics.file(
+//   "W32/sBotics_Data/StreamingAssets/ColorTheme.json.zip",
+//   (err, resp) => {
+//     if (err) return console.log(err);
+//     console.log(resp);
+//   }
+// );
+
+// sbotics.file(
+//   "W32/sBotics_Data/StreamingAssets/Addons/BlockEduc.exe",
+//   (err, resp) => {
+//     if (err) return console.log(err);
+//     console.log(resp);
+//   }
+// );
